557cb69168ebd35e5ac5764fe1c9f72c8b73e5ed
Sam Taggart <sam@sasworkshops.com>
2024-08-01 09:24:13 -0600

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:python/src/exampleB.py

--- python/src/exampleB.py
+++ python/src/exampleB.py
@@ -1,7 +1,8 @@
 
 def find_quantity(basket, item_name):
-    raise NotImplementedError()
-
+    if item_name in basket:
+        return basket[item_name]["quantity"]
+    return 0
 
 def calculate_total(basket):
     raise NotImplementedError()
--- python/tests/test_exampleB.py
+++ python/tests/test_exampleB.py
@@ -2,7 +2,34 @@ import pytest
 
 from exampleB import find_quantity, calculate_total
 
+"""
+    Empty basket - basket contains no items
+    one item "A" - basket contains 1 item "A"
+    two items "A" - basket contains 2 items "A"
+    two items, "A" and "B" - basket contains 1 item "A"
+    Empty basket - total price 0
+    "A" costs $10, basket contains one "A" - total $10
+    "D" costs $160, basket contains one "D" - total $152 (5% discount)
+    "E" costs $250, basket contains one "E" - total $225 (10% discount)
+    Basket contains two "D" - total $288 (10% discount)
+    Remove the "Ignore" marking on the original test and hopefully it will pass!
+"""
 
+def test_empty_basket():
+    basket = { }
+    assert find_quantity(basket, "A") == 0
+
+def test_contains_one_item():
+    basket = {
+    "A": {"quantity": 1, "price": 10},
+  }
+    assert find_quantity(basket, "A") == 1
+
+def test_when_item_quantity_greater_than_one():
+    basket = {
+        "A": {"quantity": 5, "price": 10},
+    }
+    assert find_quantity(basket, "A") == 5
 @pytest.mark.skip(reason="Not implemented yet")
 def test_total_over_100_gives_five_percent_discount():
     basket = {
